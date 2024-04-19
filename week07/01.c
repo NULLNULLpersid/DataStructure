d089898e78a2afa27b7d0c52468fbb09b2a83479
NULLNULLpersid <persid@cu.ac.kr>
2024-04-19 11:23:10 +0900

    week07

--- week07/01.c
+++ week07/01.c
@@ -55,22 +55,20 @@ int main(void) {
     printf("%d > %d", root->data, root->node->data);
     return 0;
 }
-*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct TreeNode {
     int data;
-    struct TreeNode *left, *right, *node;
+    struct TreeNode *left, *right, *parent;
 } TreeNode;
 
 TreeNode *NodeCreate(TreeNode *n, int data) {
     TreeNode *node = (TreeNode*)malloc(sizeof(TreeNode));
     node->data = data;
-    node->left = node->right = node->node = NULL;
-    if (n != NULL) {
-        node->node = n;
-    }
+    node->left = node->right = node->parent = NULL;
+    if (n != NULL) node->parent = n;
     return node;
 }
 
@@ -86,7 +84,7 @@ TreeNode* NodeLink(TreeNode *n, int r, int l){
         return node;
     }
     else {
-            return NodeLink(n->node, r, l);
+            return NodeLink(n->parent, r, l);
     }
 }
 
@@ -98,29 +96,31 @@ void TreeNodeCreate(TreeNode *r, int num) {
         if (rightnode->data == left) {
             rightnode = NodeLink(rightnode, left, right);
         }
-        else if (leftnode->data == right) {
+        if (leftnode->data == right) {
             leftnode = NodeLink(leftnode, left, right);
         }
         else {
-            if (rightnode->node->data == left) {
-                rightnode = NodeLink(rightnode->node, left, right);
-            }
-            else if (leftnode->node->data == right) {
-                leftnode = NodeLink(leftnode->node, left, right);
+            if (rightnode->parent != NULL && leftnode->parent != NULL) {
+                if (rightnode->parent->data == right) {
+                    rightnode = NodeLink(rightnode->parent, left, right);
+                }
+                if (leftnode->parent->data == left) {
+                    leftnode = NodeLink(leftnode->parent, left, right);
+                }
             }
         }
     }
 }
 
-int FindNode(TreeNode *r, int i) {
+TreeNode * FindNode(TreeNode *r, int i) {
     if (r != NULL) {
-        if (r->data == i) return r->node->data;
-        int left = FindNode(r->left, i);
-        if (left != 0) return left;
-        int right = FindNode(r->right, i);
-        if (right != 0) return right;
+        if (r->data == i) return r;
+        TreeNode * left = FindNode(r->left, i);
+        if (left != NULL) return left;
+        TreeNode * right = FindNode(r->right, i);
+        if (right != NULL) return right;
     }
-    return 0;
+    return NULL;
 }
 
 int main(void) {
@@ -131,7 +131,82 @@ int main(void) {
     TreeNodeCreate(root, number);
 
     for (int i = 2; i<=number;i++) {
-        printf("%d > %d\n", i, FindNode(root, i));
+        printf("%d > %d\n", i, FindNode(root, i)->parent->data);
     }
+    return 0;
+}
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode {
+    int data;
+    struct TreeNode *left, *right, *parent;
+} TreeNode;
+
+TreeNode *NodeCreate(int data) {
+    TreeNode *n = (TreeNode*)malloc(sizeof(TreeNode));
+    n->data = data;
+    n->left = n->right = n->parent = NULL;
+    return n;
+}
+
+TreeNode **NodeList(int num) {
+    TreeNode **l = (TreeNode**)malloc(sizeof(TreeNode) * num);
+    for (int i = 0; i < num; i++) l[i] = NodeCreate(i+1);
+    return l;
+}
+
+void NodeLink(TreeNode *p, TreeNode *n, int num, int d, int true){
+}
+
+TreeNode* TreeNodeCreate(TreeNode **list, int num) {
+    TreeNode *root = list[0];
+    int left, right;
+    
+    for (int i = 1; i < num; i++) {    
+        scanf("%d %d", &left, &right);
+        if (root->data == left) {
+
+        }
+        if (root->data == right) {
+
+        }
+    }
+    return root;
+}
+
+TreeNode * FindTreeNode(TreeNode *r, int i) {
+    if (r != NULL) {
+        if (r->data == i) return r;
+        TreeNode * left = FindNode(r->left, i);
+        if (left != NULL) return left;
+        TreeNode * right = FindNode(r->right, i);
+        if (right != NULL) return right;
+    }
+    return NULL;
+}
+
+void print(TreeNode *r, int num) {
+    for (int i = 2; i<=num;i++) {
+        printf("%d > %d\n", i, FindTreeNode(r, i)->parent->data);
+    }
+}
+
+void FreeList(TreeNode **list, int num) {
+    for (int i = 0; i < num; i++) {
+        free(list[i]);
+    }
+    free(list);
+}
+
+int main(void) {
+    int number;
+    scanf("%d", &number);
+    TreeNode **list = NodeList(number);
+    TreeNode * root = TreeNodeCreate(list, number);
+
+    print(root, number);
+    FreeList(list, number);
     return 0;
 }
