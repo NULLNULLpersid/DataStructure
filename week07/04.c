b3c7570b6e1bea2d520fff6ea509844ae5e6d339
NULLNULLpersid <persid@cu.ac.kr>
2024-04-22 04:26:07 +0900

    week07

---
+++ week07/04.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode {
+    int data;
+    struct TreeNode* left, *right;
+}TreeNode;
+
+TreeNode *CreateNode(int data) {
+    TreeNode *node = (TreeNode*)malloc(sizeof(TreeNode));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void PreorderTraversal(TreeNode* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        PreorderTraversal(root->left);
+        PreorderTraversal(root->right);
+    }
+}
+void InorderTraversal(TreeNode* root) {
+    if (root != NULL) {
+        InorderTraversal(root->left);
+        printf("%d ", root->data);
+        InorderTraversal(root->right);
+    }
+}
+void PostorderTraversal(TreeNode* root) {
+    if (root != NULL) {
+        PostorderTraversal(root->left);
+        PostorderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main(void) {
+    TreeNode *root = CreateNode(15);
+    TreeNode *node1 = CreateNode(4);
+    TreeNode *node2 = CreateNode(20);
+    TreeNode *node3 = CreateNode(1);
+    TreeNode *node4 = CreateNode(16);
+    TreeNode *node5 = CreateNode(25);
+    root->left = node1;
+    root->right = node2;
+    root->left->left = node3;
+    root->right->left = node4;
+    root->right->right = node5;
+
+    InorderTraversal(root);
+    printf("\n");
+    PreorderTraversal(root);
+    printf("\n");
+    PostorderTraversal(root);
+
+    return 0;
+}
