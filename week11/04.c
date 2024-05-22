252051726634d55b90d51237fb3be4d2568cfda1
NULLNULLpersid <persid@cu.ac.kr>
2024-05-23 07:27:46 +0900

    week11

---
+++ week11/04.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#define INF 99999
+
+void floydWarshall(int N, int graph[N][N]) {
+    int dist[N][N];
+
+    // Initialize the solution matrix same as input graph matrix
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    // Adding vertices individually
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print the shortest distance matrix
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (dist[i][j] == INF) {
+                printf("INF ");
+            } else {
+                printf("%d ", dist[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int graph[N][N];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    floydWarshall(N, graph);
+
+    return 0;
+}
