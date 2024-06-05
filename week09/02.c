f5604b1720accec75f4b12bde46abea10195e157
NULLNULLpersid <persid@cu.ac.kr>
2024-06-05 18:39:56 +0900

    week09

--- week09/02.c
+++ week09/02.c
@@ -3,16 +3,16 @@
 
 typedef struct {
     int *element;  // 요소 배열
-    int heap_size; // 현재 힙의 크기
-    int capacity;  // 최대 용량
+    int now_size; // 현재 힙의 크기
+    int max_size;  // 최대 용량
 } HeapType;
 
 // 힙 생성
-HeapType* create_heap(int capacity) {
+HeapType* create_heap(int max_size) {
     HeapType *heap = (HeapType*)malloc(sizeof(HeapType));
-    heap->element = (int*)malloc(sizeof(int) * capacity);
-    heap->heap_size = 0;
-    heap->capacity = capacity;
+    heap->element = (int*)malloc(sizeof(int) * max_size);
+    heap->now_size = 0;
+    heap->max_size = max_size;
     return heap;
 }
 
@@ -24,7 +24,7 @@ void free_heap(HeapType *heap) {
 
 // 요소 삽입
 void insert_max_heap(HeapType *heap, int item) {
-    int i = ++heap->heap_size;
+    int i = ++heap->now_size;
     while ((i != 1) && (item > heap->element[i / 2])) {
         heap->element[i] = heap->element[i / 2];
         i /= 2;
@@ -38,13 +38,12 @@ int delete_max_heap(HeapType *heap) {
     int item, temp;
 
     item = heap->element[1];
-    temp = heap->element[heap->heap_size--];
+    temp = heap->element[heap->now_size--];
     parent = 1;
     child = 2;
 
-    while (child <= heap->heap_size) {
-        if ((child < heap->heap_size) && (heap->element[child] < heap->element[child + 1]))
-            child++;
+    while (child <= heap->now_size) {
+        if ((child < heap->now_size) && (heap->element[child] < heap->element[child + 1])) child++;
         if (temp >= heap->element[child]) break;
 
         heap->element[parent] = heap->element[child];
@@ -57,8 +56,8 @@ int delete_max_heap(HeapType *heap) {
 }
 
 int main() {
-    int capacity = 10; // 최대 용량
-    HeapType *heap = create_heap(capacity);
+    int max_size = 10; // 최대 용량
+    HeapType *heap = create_heap(max_size);
 
     // 힙에 요소 삽입
     insert_max_heap(heap, 99);
@@ -71,9 +70,7 @@ int main() {
     insert_max_heap(heap, 9);
 
     // 힙에서 최대값 삭제
-    for (int i = 0; i < 8; i++) {
-            printf("%d ", delete_max_heap(heap));
-    }
+    for (int i = 0; i < 8; i++) printf("%d ", delete_max_heap(heap));
 
     free_heap(heap);
     return 0;
